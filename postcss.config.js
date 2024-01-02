31f77da92538c564d245ea4d453f07db0afaa1a4
Tomxuetao <15555030339@163.com>
2024-01-02 18:04:41 +0800

    style: 💄 修改autoprefixer配置
    修改autoprefixer配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
     autoprefixer: {
+      grid: false,
       overrideBrowserslist: [
         'Android 4.1',
         'iOS 7.1',
@@ -8,8 +9,7 @@ module.exports = {
         'ff > 31',
         'ie >= 8',
         'last 10 versions'
-      ],
-      grid: true
+      ]
     },
     'postcss-px-to-viewport-8-plugin': {
       viewportWidth: 375
